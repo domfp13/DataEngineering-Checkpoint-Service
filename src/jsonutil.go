289294b80acc44be91b3a6bc9d3cc40a441e075a
Enrique Plata <>
2022-01-09 19:44:20 +0100

    Adding write utility

--- src/jsonutil.go
+++ src/jsonutil.go
@@ -2,6 +2,7 @@ package src
 
 import (
 	"encoding/json"
+	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
 )
@@ -27,4 +28,35 @@ func ReadFileJsonObject(objectName string) []checkpointObject {
 	}
 
 	return checkpointObjects
+}
+
+// WriteFileJsonObject uses marshalling to write a struct into a JSON object
+// The file save under tmp/checkpoint/
+func WriteFileJsonObject(objectName string, c *gin.Context) bool {
+
+	var newCheckpoint checkpointObject
+	var checkpointObjects []checkpointObject
+
+	if err := c.Bind(&newCheckpoint); err != nil {
+		log.Println(err)
+	}
+
+	// makes sure the content was passed correctly
+	if newCheckpoint.Time == "" {
+		return false
+	}
+
+	checkpointObjects = append(checkpointObjects, newCheckpoint)
+
+	content, err2 := json.Marshal(checkpointObjects)
+	if err2 != nil {
+		log.Println("Error JSON Marshalling")
+		log.Println(err2.Error())
+	}
+
+	err3 := ioutil.WriteFile(localDir+prefix+objectName+extension, content, 0644)
+	if err3 != nil {
+		log.Println(err3)
+	}
+	return true
 }
